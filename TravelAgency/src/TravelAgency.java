0fe4dd9ad00cb0338c17e20ad4b897c0570687b8
Rachel <rachelibr050@gmail.com>
2022-10-31 14:47:01 +0200

    created travel agency exercise

--- TravelAgency/src/TravelAgency.java
+++ TravelAgency/src/TravelAgency.java
@@ -6,13 +6,13 @@ import java.util.Map;
 public class TravelAgency {
     VehicleFactory vehicleFactory;
     List<Vehicle> availableVehicles;
-    Map<Passenger, Vehicle> usedVehicles;
+    Map<Vehicle, Passenger> usedVehicles;
 
 
     public TravelAgency() {
-        vehicleFactory=VehicleFactory.getInstance();
-        usedVehicles=new HashMap<>();
-        availableVehicles =new ArrayList<>();
+        vehicleFactory = VehicleFactory.getInstance();
+        usedVehicles = new HashMap<>();
+        availableVehicles = new ArrayList<>();
         availableVehicles.add(vehicleFactory.createVehicle(VehicleType.PLANE));
         availableVehicles.add(vehicleFactory.createVehicle(VehicleType.BUS));
         availableVehicles.add(vehicleFactory.createVehicle(VehicleType.BUS));
@@ -30,22 +30,22 @@ public class TravelAgency {
         availableVehicles.add(vehicleFactory.createVehicle(VehicleType.BOAT));
         availableVehicles.add(vehicleFactory.createVehicle(VehicleType.BOAT));
     }
-    public void assignVehicle(Passenger passenger){
-        if(availableVehicles.isEmpty()){
+
+    public void assignVehicle(Passenger passenger) {
+        if (availableVehicles.isEmpty()) {
             throw new ArrayIndexOutOfBoundsException("there are no more available vehicles");
         }
-        if(availableVehicles.stream().anyMatch(v->v.getType().equals(passenger.getFavoriteVehicle()))){
-           Vehicle vehicle= availableVehicles.stream().filter(v->v.getType().equals(passenger.getFavoriteVehicle())).findFirst().get();
-           availableVehicles.remove(vehicle);
-           usedVehicles.put(passenger, vehicle);
-        }
-        else{
-            Vehicle vehicle=availableVehicles.get(0);
+        if (availableVehicles.stream().anyMatch(v -> v.getType().equals(passenger.getFavoriteVehicle()))) {
+            Vehicle vehicle = availableVehicles.stream().filter(v -> v.getType().equals(passenger.getFavoriteVehicle())).findFirst().get();
+            availableVehicles.remove(vehicle);
+            usedVehicles.put(vehicle, passenger);
+        } else {
+            Vehicle vehicle = availableVehicles.get(0);
             availableVehicles.remove(0);
-            usedVehicles.put(passenger, vehicle);
+            usedVehicles.put(vehicle, passenger);
         }
-        if(availableVehicles.isEmpty()){
-            usedVehicles.forEach((p,v)->v.transport(p));
+        if (availableVehicles.isEmpty()) {
+            usedVehicles.forEach((v, p) -> v.transport(p));
         }
     }
 }
